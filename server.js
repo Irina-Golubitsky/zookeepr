e435f0b2274b5568983fedbdded9c99491199550
Irina-Golubitsky <irina.golubitsky@gmail.com>
2021-08-17 10:54:05 -0700

    post endpoint

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.post('/api/animals', (req, res) => {
     res.status(400).send('The animal is not properly formatted.');
   } else {
     const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
+    res.json(animal)
   }
 });
 app.listen(PORT, () => {
