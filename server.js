7f3882c191491cdf29fb90e50e6291da2f090d99
Irina-Golubitsky <irina.golubitsky@gmail.com>
2021-08-16 19:16:47 -0700

    add server 2 endpoints

--- server.js
+++ server.js
@@ -40,6 +40,10 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
   app.listen(PORT, () => {
     console.log(`API  server now on port ${PORT}!`);
   });
@@ -49,4 +53,12 @@ function filterByQuery(query, animalsArray) {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
   });
